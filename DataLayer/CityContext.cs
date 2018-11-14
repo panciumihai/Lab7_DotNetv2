e6a10416db5f999480ad9f54433b4179c905897f
Panciu-Rusu Mihai <panciu.rusu.mihai@gmail.com>
2018-11-14 11:45:53 +0200

    I forgot the CityContext
    This is the real final commit, sorry again..

---
+++ DataLayer/CityContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public class CityContext: DbContext
+    {
+        public CityContext(DbContextOptions<CityContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<City> Cities { get; set; }
+    }
+}
