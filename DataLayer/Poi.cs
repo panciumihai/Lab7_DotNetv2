633642ef2d4516b4400acc3e2b176949288fa75c
Codrut Iftimie <gabriel.iftimie@info.uaic.ro>
2018-11-14 10:53:50 +0200

    DataLayer Poi
    Poi.cs
    PoiContext.cs

---
+++ DataLayer/Poi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataLayer
+{
+    public class Poi
+    {
+        public Guid PoiId { get; set; }
+        [MinLength(50)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid CityId { get; set; }
+    }
+}
---
+++ DataLayer/PoiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer
+{
+    public sealed class PoiContext : DbContext
+    {
+        public PoiContext(DbContextOptions<PoiContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Poi> Pois { get; set; }
+    }
+}
