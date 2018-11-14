c4e7e0ec0df30e41e0b640d2969285939732f567
Panciu-Rusu Mihai <panciu.rusu.mihai@gmail.com>
2018-11-14 10:49:47 +0200

    DataLayer

---
+++ DataLayer/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLayer
+{
+    class City
+    {
+        public Guid CityId { get; set; }
+        public string Name { get; set; }
+        [MinLength(50)]
+        public string Description { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
