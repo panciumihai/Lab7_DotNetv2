c4c4923c990f5927a43b173241cb7f17a736e8e0
Codrut Iftimie <gabriel.iftimie@info.uaic.ro>
2018-11-14 10:59:14 +0200

    Poi Repository

---
+++ BusinessLayer/PoiRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class PoiRepository
+    {
+        private readonly PoiContext _context;
+
+        public PoiRepository(PoiContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Poi city)
+        {
+            city.PoiId = new Guid();
+            _context.Pois.Add(city);
+            _context.SaveChanges();
+        }
+
+        public void Edit(Guid id, Poi city)
+        {
+            _context.Pois.Update(city);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Guid guid)
+        {
+            var result = _context.Pois.FirstOrDefault(c => c.PoiId == guid);
+            if (result != null)
+            {
+                _context.Pois.Remove(result);
+                _context.SaveChanges();
+            }
+        }
+
+        public Poi Get(Guid guid)
+        {
+            return _context.Pois.FirstOrDefault(c => c.PoiId == guid);
+        }
+
+        public IReadOnlyCollection<Poi> GetAll()
+        {
+            return _context.Pois.ToList();
+        }
+    }
+}
