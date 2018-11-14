631b09cc3d5abed75448c087c4df8b65f24f0a3c
Codrut Iftimie <gabriel.iftimie@info.uaic.ro>
2018-11-14 11:29:13 +0200

    PoiModel And PoisController

---
+++ Lab7_DotNet/Controllers/PoiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BusinessLayer;
+using DataLayer;
+using System.Collections.Generic;
+using System;
+
+namespace Lab7_DotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoisController : ControllerBase
+    {
+        private readonly IPoiRepository _repository;
+
+        public PoisController(IPoiRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IReadOnlyList<Poi>> Get()
+        {
+            return Ok(this._repository.GetAll());
+        }
+
+        [HttpGet("{id}", Name = "GetById")]
+        public ActionResult<Poi> Get(Guid id)
+        {
+            return Ok(this._repository.Get(id));
+        }
+        [HttpPost]
+        public ActionResult<Poi> Post([FromBody] CreatePoiModel createPoiModel)
+        {
+            if (createPoiModel == null)
+            {
+                return BadRequest();
+            }
+
+            Poi todo = new Poi(createPoiModel.Name, createPoiModel.Description);
+            this._repository.Create(todo);
+
+            return CreatedAtRoute("GetById", new { id = todo.PoiId }, todo);
+        }
+    }
+}
---
+++ Lab7_DotNet/Models/CreatePoiModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab7_DotNet
+{
+    public class CreatePoiModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(50)]
+        public string Description { get; set; }
+    }
+}
